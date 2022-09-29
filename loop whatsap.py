d699671db2a422cf2f16d383143e1d48ffa4783d
Darlan Wilson <74836472+Darlansc15@users.noreply.github.com>
2022-09-29 16:21:42 -0300

    Update loop whatsap.py

--- loop whatsap.py
+++ loop whatsap.py
@@ -5,7 +5,6 @@ time.sleep(10)
 for i in range(100):
   pg.write('darlan')
   pg.press('Enter')
-  landarlan
   darlan
   darlan
   darlan
@@ -101,6 +100,8 @@ for i in range(100):
   darlan
   darlan
   darlan
-  DARLAN
-  DARLAN
+  darlan
+  darlan
+  darlan
+  darlan
   darlan
